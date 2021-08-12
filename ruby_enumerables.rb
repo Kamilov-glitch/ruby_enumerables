e488f5d24049c0e309e669852ee762db40bb15fb
https://github.com/Kamilov-glitch <kamilovfuad@gmail.com>
2021-08-12 19:33:35 +0400

    Make custom each methods

--- ruby_enumerables.rb
+++ ruby_enumerables.rb
@@ -1,7 +1,22 @@
 module Enumerable
 
   def my_each
-    
+    if block_given?
+      for i in 0..self.length - 1
+        yield(self[i])
+      end
+    end
+  end
+
+  def my_each_with_index
+    if block_given?
+      for i in 0..self.length - 1
+        yield(self[i], i)
+      end
+    end
   end
 
 end
+
+[1, 3, 3].my_each{ |i| puts "hi: #{i}"} 
+[1, 3, 3].my_each_with_index{ |i, indx| puts "hi: #{i}, #{indx}"} 
